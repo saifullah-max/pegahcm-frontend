461f11b57a724a475d8523f4db10628b1de91dbb
Saifullah Ahmed <saifullahahmed380@gmail.com>
2025-08-24 15:47:39 +0500

    initial commit with latest frontend code

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,10 +1,10 @@
 // src/utils/socket.ts
-import { io, Socket } from 'socket.io-client';
+import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = 'http://localhost:3003'; // Replace with your backend URL if deployed
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL; // Replace with your backend URL if deployed
 
 const socket: Socket = io(SOCKET_URL, {
-    autoConnect: false, // You’ll manually connect
+  autoConnect: false, // You’ll manually connect
 });
 
 export default socket;
