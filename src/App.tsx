bcd81849f7a67c5c8c2701b93896c1177f7c9b84
Saifullah Ahmed <saifullahahmed380@gmail.com>
2025-08-24 18:56:06 +0500

    socket fixed

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ function App() {
       <SocketProvider>
         <ThemeProvider>
           <AuthInitializer />
-          <Router basename="/projects/pegahcm">
+          <Router basename="/project/pegahcm">
             <Routes>
               {/* Auth Routes */}
               <Route path="/login" element={<Login />} />
--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  base: "/projects/pegahcm/",
+  base: "/project/pegahcm/",
 });
